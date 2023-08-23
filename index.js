676fda2ed88eab3a6319be1691957e1ef6192fd2
daniwalter001 <daniwalter001@gmail.com>
2023-08-23 15:32:47 +0000

    M

--- index.js
+++ index.js
@@ -483,7 +483,7 @@ app
     });
 
     // result = result?.length >= 10 ? result.splice(-10) : result;
-    // result = result?.length >= 20 ? result.splice(-20) : result;
+    result = result?.length >= 20 ? result.splice(-20) : result;
     result.reverse();
 
     // console.log({ result });
