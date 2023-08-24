b3c71b2b3cb2c4cf087e40b2074e603b81218f3d
daniwalter001 <daniwalter001@gmail.com>
2023-08-24 17:08:19 +0000

    Debug

--- index.js
+++ index.js
@@ -566,7 +566,7 @@ app
     console.log("------------------------------------");
 
     // result = result?.length >= 10 ? result.splice(-10) : result;
-    result = result?.length >= 20 ? result.splice(-20) : result;
+    // result = result?.length >= 20 ? result.splice(-20) : result;
     result.reverse();
 
     // console.log({ result });
